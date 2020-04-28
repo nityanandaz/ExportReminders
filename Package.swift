079daa39dc8685bb23cb1b4ceadcff5333d5dbf1
Nityananda Zbil <nityananda.zbil@icloud.com>
2020-04-28 20:08:16 +0200

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ExportReminders",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.0.1")),
+    ],
+    targets: [
+        .target(
+            name: "ExportReminders",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]
+        ),
+        .testTarget(
+            name: "ExportRemindersTests",
+            dependencies: ["ExportReminders"]),
+    ]
+)
